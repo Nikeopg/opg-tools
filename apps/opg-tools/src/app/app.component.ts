f3237a9e34417d3dc23edc96869065dc6b2ca0ef
Nikita <vns95@bk.ru>
2024-12-09 23:45:41 +0300

    initial project

--- apps/opg-tools/src/app/app.component.ts
+++ apps/opg-tools/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { TodoListComponent } from '@opg-tools/todo-list';
 
 @Component({
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule, TodoListComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.less',
---
+++ libs/app-todo-list/feature-todo-list/src/lib/todo-list/todo-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-todo-list',
+  imports: [CommonModule],
+  templateUrl: './todo-list.component.html',
+  styleUrl: './todo-list.component.css',
+})
+export class TodoListComponent {}
